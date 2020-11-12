cbb78a134ccac7228b845c87b0f061d80786cf31
abhijit shingote <abhijitshingote@gmail.com>
2020-11-12 15:35:55 +0000

    improvements to documentation

--- README.md
+++ README.md
@@ -3,6 +3,35 @@ Database provides the data infrastructure to consume Sparkify app logs in the fo
 
 ## Database Schema Design
 Database follows conventional design principles so we have dimension tables for artists, songs, users and a fact table that logs users interaction with the songs. ETL pipeline consumes songs/artists data and app log data. Both are in the form of json documents.
+#### artists table
+![Alt text](images/artisttable.png?raw=true "Title")
+
+#### users table
+![Alt text](images/usertable.png?raw=true "Title")
+
+#### song table
+![Alt text](images/songtable.png?raw=true "Title")
+
+#### time table
+![Alt text](images/timetable.jpg?raw=true "Title")
+
+#### songplay table
+![Alt text](images/songplaytable.png?raw=true "Title")
+
+## Files
+create_tables.py  - Creates database and then the subsequent tables by reading sql_queries.py
+sql_queries.py - Create and insert queries to be used in create_tables.py and etl.py
+etl.py - After tables have been created after running create_tables.py, use this script to process files and insert records from data/.
+test.ipynb - test if records are available in the tables
+etl.ipynb - can be ignored. used to test functionality of etl.py
+data/ - repo of files to be processed
+images/ - ignore
+
+## Instructions
+```
+1.  python3 create_tables.py'     - Creates database and then the subsequent tables by reading sql_queries.py
+2.  python3 etl.py   - process files and insert records from data/
+```
 
 ## Example Queries
 #### Top 5 most popular artist
--- sql_queries.py
+++ sql_queries.py
@@ -8,37 +8,42 @@ time_table_drop = "drop table if exists time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""create table if not exists songplays (songplay_id serial, start_time bigint, user_id int, level text, song_id text, artist_id text, session_id int, location text, user_agent text)
+songplay_table_create = ("""create table if not exists songplays (songplay_id serial primary key, start_time bigint not null, user_id int not null references users(user_id), level text, 
+song_id text references songs(song_id), artist_id text references artists(artist_id), session_id int, location text, user_agent text,
+CONSTRAINT uniq_usersongstarttime UNIQUE (user_id, song_id, start_time))
 """)
 
-user_table_create = ("""create table if not exists users (user_id int, first_name text, last_name text, gender text, level text)
+user_table_create = ("""create table if not exists users (user_id int primary key, first_name text, last_name text, gender text, level text)
 """)
 
-song_table_create = ("""create table if not exists songs (song_id text, title text, artist_id text, year int, duration float)
+song_table_create = ("""create table if not exists songs (song_id text primary key, title text, artist_id text, year int, duration float)
 """)
 
-artist_table_create = ("""create table if not exists artists (artist_id text, name text, location text, latitude float, longitude float)
+artist_table_create = ("""create table if not exists artists (artist_id text primary key, name text, location text, latitude float, longitude float)
 """)
 
-time_table_create = ("""create table if not exists time (start_time numeric, hour int, day int, week int, month int, year int, weekday int)
+time_table_create = ("""create table if not exists time (start_time numeric primary key, hour int, day int, week int, month int, year int, weekday int)
 """)
+
+
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""insert into songplays ( start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) values ( %s,%s,%s,%s,%s,%s,%s,%s)
+ON CONFLICT (user_id, song_id, start_time) DO NOTHING
 """)
 
-user_table_insert = ("""insert into users (user_id, first_name, last_name, gender, level) values(%s,%s,%s,%s,%s)
+user_table_insert = ("""insert into users (user_id, first_name, last_name, gender, level) values(%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
-song_table_insert = ("""insert into songs (song_id, title, artist_id, year, duration) values (%s,%s,%s,%s,%s)
+song_table_insert = ("""insert into songs (song_id, title, artist_id, year, duration) values (%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING
 """)
 
-artist_table_insert = ("""insert into artists (artist_id, name, location, latitude, longitude) values (%s,%s,%s,%s,%s)
+artist_table_insert = ("""insert into artists (artist_id, name, location, latitude, longitude) values (%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING
 """)
 
 
-time_table_insert = ("""insert into time (start_time, hour, day, week, month, year, weekday) values (%s,%s,%s,%s,%s,%s,%s)
+time_table_insert = ("""insert into time (start_time, hour, day, week, month, year, weekday) values (%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
@@ -53,5 +58,5 @@ and s.duration = %s
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [ user_table_create, song_table_create, artist_table_create, time_table_create,songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
